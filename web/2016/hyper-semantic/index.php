675d638713b3a4fdf26c7e66daef707ee8833d5c
Joby Elliott <Joby Elliott>
2016-09-06 18:58:28 -0600

    Added hyper-semantic demo

---
+++ web/2016/hyper-semantic/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <title>Experimental Projects by Joby</title>
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/branding-head.inc'); ?>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1" />
+    <meta name="viewport" content="initial-scale=1, minimum-scale=1, maximum-scale=1.5" />
+</head>
+<body>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/branding-block.inc'); ?>
+    
+    <div id="experimental-page">
+        <h1>Hyper semantic HTML/CSS framework</h1>
+        <p>This is a proof of concept for building a website with absolutely zero non-semantic markup. Not even CSS classes. Every single style rule is based around the proper semantic use of HTML5 tags.</p>
+        <iframe src="hyper-semantic.html" style="width:100%;height:25em;border:1px solid #ccc;"></iframe>
+    </div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/branding-bottom.inc'); ?>
+</body>
+</html>
---
+++ web/2016/template.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+    <title>Experimental Projects by Joby</title>
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/branding-head.inc'); ?>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1" />
+    <meta name="viewport" content="initial-scale=1, minimum-scale=1, maximum-scale=1.5" />
+    <link href="/layout/jquery-ui-1.11.1/jquery-ui.css" rel="stylesheet">
+</head>
+<body>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/branding-block.inc'); ?>
+    
+    <div id="experimental-page">
+        <h1>Page title</h1>
+    </div>
+
+<script>
+$(function(){
+})
+</script>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/branding-bottom.inc'); ?>
+</body>
+</html>
